708cedaf963f44c82b4e892f9eb3fc9cc5f304cd
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-20 01:14:55 +1000

    Added functions for sampling service time

--- utilities.py
+++ utilities.py
@@ -12,3 +12,10 @@ def inter_arrival_time():
     a1 = random.expovariate(1/0.85)
     a2 = random.uniform(0.05, 0.25)
     return a1+a2
+
+def pre_processor_service_time(n):
+    return random.expovariate(10/n)
+
+def sub_task_service_time(n):
+    k0 = 20*(1.08/2.08)/pow(n, 1.65)
+    return random.paretovariate(2.08)*k0
