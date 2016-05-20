74df51ec02ab7df8c77e45d1e616f23e074dd718
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-20 13:45:30 +1000

    Simulation can now run for either specified number of requests or certain amount of time

--- simulation.py
+++ simulation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import argparse
 import sys
 import random
 import utilities
@@ -17,15 +18,21 @@ class Simulator:
         self.next_arrival = utilities.inter_arrival_time()
         self.preprocessing_time = 0
         self.job_id = 0
+        self.completed_requests = 0
 
-    def simulate(self, requests=10):
+    def simulate(self, requests=10, time=None):
         self.time = self.next_arrival
         self.request_arrived()
         waiting_time = min(self.next_arrival, self.preprocessing_time)
 
-        while requests:
-            self.time += waiting_time
-            waiting_time = self.recalculate_times(waiting_time)
+        if time:
+            while self.time < time:
+                self.time += waiting_time
+                waiting_time = self.recalculate_times(waiting_time)
+        else:
+            while requests > self.completed_requests:
+                self.time += waiting_time
+                waiting_time = self.recalculate_times(waiting_time)
 
     def seed(self, seed):
         random.seed(seed)
@@ -35,12 +42,11 @@ class Simulator:
 
         for s in range(self.m):
             server = self.servers[s]
-            print(server)
             if server:
                 server[0][1] -= waited_time
                 if not server[0][1]:
                     self.server_finish_sub_task(s)
-                if server[0][1]:
+                if server:
                     if not waiting_time:
                         waiting_time = server[0][1]
                     else:
@@ -55,7 +61,6 @@ class Simulator:
                     waiting_time = self.preprocessing_time
                 else:
                     waiting_time = min(waiting_time, self.preprocessing_time)
-            print("Pre-processing time: %s" % self.preprocessing_time)
 
         self.next_arrival -= waited_time
         if not self.next_arrival:
@@ -64,7 +69,6 @@ class Simulator:
             waiting_time = self.next_arrival
         else:
             waiting_time = min(waiting_time, self.next_arrival)
-        print("Arrival time: %s" % self.next_arrival)
 
         return waiting_time
 
@@ -91,20 +95,28 @@ class Simulator:
     def server_finish_sub_task(self, i):
         (job_id, _) = self.servers[i].pop(0)
         self.join[job_id] -= 1
+        self.log("Server %d finished subtask on job %d" % (i, job_id))
+
         if not self.join[job_id]:
             self.join.pop(job_id)
-
-        self.finished_job(job_id)
-
-    def finished_job(self, job_id):
-        self.log("Finished job number %d" % (self.time, job_id))
+            self.log("Job number %d completed" % job_id)
+            self.completed_requests += 1
 
     def log(self, message):
         print("%s - %s" % (self.time, message))
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    mode_group = parser.add_mutually_exclusive_group()
+    mode_group.add_argument('--requests', '-r', type=int, default=10, help='number of requests to process')
+    mode_group.add_argument('--time', '-t', type=int, help='run simulation for specified amount of time')
+    args = parser.parse_args()
+    return args
+
 def main():
+    args = parse_args()
     simulator = Simulator(5)
-    simulator.simulate()
+    simulator.simulate(args.requests, args.time)
 
 if __name__ == '__main__':
     main()
