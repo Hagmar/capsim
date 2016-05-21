e9e05dac3d37190aa81d3e93c2bc41d3de2d1bb8
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-21 18:27:18 +1000

    Fixed mix-up between arrival time and arrival rate

--- utilities.py
+++ utilities.py
@@ -9,7 +9,7 @@ def inter_arrival_sequence(n=None):
             yield inter_arrival_time()
 
 def inter_arrival_time():
-    a1 = random.expovariate(1/0.85)
+    a1 = random.expovariate(0.85)
     a2 = random.uniform(0.05, 0.25)
     return a1+a2
 
