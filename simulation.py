4e553f9af20ba20a56b57e50d4ab64fb18812677
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-21 14:07:56 +1000

    Server sub-task times are now calculated when the server starts working on it A new request arrival no longer causes pre-processing time to change When the pre-processor finishes a task, immediately starts working on the next if any exist Changed default behavior to quiet, allowing a verbose-flag Changed logging format

--- simulation.py
+++ simulation.py
@@ -14,6 +14,7 @@ class Simulator:
         self.preprocessor = []
         self.join = {}
         self.servers = []
+        self.server_times = [0]*self.m
         for i in range(self.m):
             self.servers.append([])
         self.next_arrival = utilities.inter_arrival_time()
@@ -23,8 +24,8 @@ class Simulator:
         self.job_start_times = {}
         self.job_response_times = []
 
-    def simulate(self, requests=10, time=None, q=False):
-        self.q = q
+    def simulate(self, requests=10, time=None, v=False):
+        self.v = v
         self.time = self.next_arrival
         self.request_arrived()
         waiting_time = min(self.next_arrival, self.preprocessing_time)
@@ -47,14 +48,14 @@ class Simulator:
         for s in range(self.m):
             server = self.servers[s]
             if server:
-                server[0][1] -= waited_time
-                if not server[0][1]:
+                self.server_times[s] -= waited_time
+                if not self.server_times[s]:
                     self.server_finish_sub_task(s)
                 if server:
                     if not waiting_time:
-                        waiting_time = server[0][1]
+                        waiting_time = self.server_times[s]
                     else:
-                        waiting_time = min(waiting_time, server[0][1])
+                        waiting_time = min(waiting_time, self.server_times[s])
 
         if self.preprocessor:
             self.preprocessing_time -= waited_time
@@ -69,6 +70,10 @@ class Simulator:
         self.next_arrival -= waited_time
         if not self.next_arrival:
             self.request_arrived()
+            if not waiting_time:
+                waiting_time = self.preprocessing_time
+            else:
+                waiting_time = min(waiting_time, self.preprocessing_time)
         if not waiting_time:
             waiting_time = self.next_arrival
         else:
@@ -77,46 +82,59 @@ class Simulator:
         return waiting_time
 
     def request_arrived(self):
+        if not self.preprocessor:
+            self.preprocessing_time = utilities.pre_processor_service_time(self.n)
+            if self.v:
+                self.log("Job {} arrived, requiring {:0.6f} time".format(self.job_id, self.preprocessing_time))
+        else:
+            if self.v:
+                self.log("Job %d arrived and was added to pre-processor queue" % self.job_id)
         self.preprocessor.append(self.job_id)
         self.next_arrival = utilities.inter_arrival_time()
-        self.preprocessing_time = utilities.pre_processor_service_time(self.n)
         self.job_start_times[self.job_id] = self.time
-
-        if not self.q:
-            self.log("Job number %d arrived, requiring %s time" % (self.job_id, self.preprocessing_time))
 
         self.job_id += 1
 
     def split_request(self):
-        job_id = self.preprocessor[0]
+        job_id = self.preprocessor.pop(0)
         selected_servers = sorted(random.sample(range(self.m), self.n))
-        if not self.q:
-            self.log("Job number %d split to servers %s" % (job_id, selected_servers))
+        if self.v:
+            self.log("Job %d split to servers %s" % (job_id, selected_servers))
 
         for s in selected_servers:
-            service_time = utilities.sub_task_service_time(self.n)
-            self.servers[s].append([job_id, service_time])
-            if not self.q:
-                self.log("Time required at server %d: %s" % (s, service_time))
-        self.preprocessor.pop(0)
+            if not self.server_times[s]:
+                service_time = utilities.sub_task_service_time(self.n)
+                self.server_times[s] = service_time
+                if self.v:
+                    self.log("S{} started subtask of job {}, requiring {:0.6f} time".format(s, job_id, service_time))
+            self.servers[s].append(job_id)
         self.join[job_id] = self.n
 
+        if self.preprocessor:
+            self.preprocessing_time = utilities.pre_processor_service_time(self.n)
+            if self.v:
+                self.log("Job {} started, requiring {:0.6f} time".format(self.preprocessor[0], self.preprocessing_time))
+
     def server_finish_sub_task(self, i):
-        (job_id, _) = self.servers[i].pop(0)
+        job_id = self.servers[i].pop(0)
         self.join[job_id] -= 1
-        if not self.q:
-            self.log("Server %d finished subtask on job %d" % (i, job_id))
+        if self.v:
+            self.log("S%d finished subtask on job %d" % (i, job_id))
+        if self.servers[i]:
+            service_time = utilities.sub_task_service_time(self.n)
+            if self.v:
+                self.log("S{} started subtask of job {}, requiring {:0.6f} time".format(i, self.servers[i][0], service_time))
 
         if not self.join[job_id]:
             self.join.pop(job_id)
             total_time = self.time - self.job_start_times.pop(job_id)
-            if not self.q:
-                self.log("Job number %d completed after %s time" % (job_id, total_time))
+            if self.v:
+                self.log("Job {} completed after {:0.6f} time".format(job_id, total_time))
             self.completed_requests += 1
             self.job_response_times.append(total_time)
 
     def log(self, message):
-        print("%s - %s" % (self.time, message))
+        print("{:0.6f} - {}".format(self.time, message))
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -125,7 +143,7 @@ def parse_args():
     mode_group = parser.add_mutually_exclusive_group()
     mode_group.add_argument('--requests', '-r', type=int, default=10, help='number of requests to process')
     mode_group.add_argument('--time', '-t', type=int, help='run simulation for specified amount of time')
-    parser.add_argument('-q', action='store_true', help="don't print log to stdout")
+    parser.add_argument('-v', action='store_true', help="don't print log to stdout")
     parser.add_argument('--seed', '-s', type=float, help='seed the simulation with a predetermined value')
     args = parser.parse_args()
     return args
@@ -143,21 +161,20 @@ def main():
         total_response_time = 0
         for i in range(args.avg):
             simulator = Simulator(args.n)
-            simulator.simulate(args.requests, args.time, args.q)
+            simulator.simulate(args.requests, args.time, args.v)
             (throughput, mean_response_time) = evaluate(simulator)
             total_throughput += throughput
             total_response_time += mean_response_time
-            print("--- Simulation %d ---" % i)
-            print("  Throughput: %s" % throughput)
-            print("  Mean response time: %s" % mean_response_time)
+#            print("--- Simulation %d ---" % i)
+#            print("  Throughput: %s" % throughput)
+#            print("  Mean response time: %s" % mean_response_time)
         total_throughput /= args.avg
         total_response_time /= args.avg
-        print()
         print("Average throughput: %s" % total_throughput)
         print("Average mean response time: %s" % total_response_time)
     else:
         simulator = Simulator(args.n)
-        simulator.simulate(args.requests, args.time, args.q)
+        simulator.simulate(args.requests, args.time, args.v)
 
 def evaluate(simulator):
     requests = simulator.completed_requests
